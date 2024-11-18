6db0e8cd22a35d48d1d820d55213be4f7379bffe
DIPANJAN <dipanjanmondal123@gmail.com>
2024-11-18 07:06:31 +0530

    there is anothere database name present in db.php file / in book_appointment.htm file change the path which is not working // doing the connection in login.html

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 // db.php - Database connection
 $servername = "localhost";   // Change this if your database is on a different server
 $username = "root";          // Change this if you're using a different username
-$password = "";              // Change this if you're using a password
-$dbname = "appointment_system"; // The name of your database
+$password = "Dipanjan@2004";              // Change this if you're using a password
+$dbname = "healthcare_management"; // The name of your database
 
 // Create a connection
 $conn = new mysqli($servername, $username, $password, $dbname);
